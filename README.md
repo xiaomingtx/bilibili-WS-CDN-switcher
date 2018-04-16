5c742dde439f350503c392b83df5570d3b231bf0
Wang Xiaoming <xiaomingtx@yeah.net>
2018-04-16 20:10:54 +0800

    修复多视频页面切换视频提示框消失bug

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # bilibili-WS-CDN-switcher
 自动使用网宿cdn，北邮校园网可以节省流量
 
-ws.acgvideo.com在北邮校园网自动被解析为10.3.200.205，不试用校外流量
+ws.acgvideo.com在北邮校园网自动被解析为10.3.200.205，不使用校外流量
 
 其他学校请自行测试ws.acgvideo.com能否解析到内网地址
 
--- bilibili-WS-CDN-switcher.js
+++ bilibili-WS-CDN-switcher.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         bilibili强制使用网宿CDN
-// @namespace    https://github.com/xiaomingtx/bilibili-WS-CDN-switcher
 // @version      0.1
 // @description  bilibili强制使用网宿CDN，北邮校园网可节省流量
 // @author       xiaomingtx
-// @include      https://*.bilibili.com*
-// @include      http://*.bilibili.com*
+// @include      *://*bilibili.com*
+// @exlucde      *://live.bilibili.com*
 // @require      https://unpkg.com/xhook@1.4.7/dist/xhook.min.js
-// @require      https://static.hdslb.com/js/jquery.min.js
+// @require      http://code.jquery.com/jquery-3.3.1.slim.min.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
 var timeOutInfo;
+var timerSetupInfo;
 var wscndError = false;
 
 //修改提示框信息
@@ -22,7 +22,35 @@ function changeInfo(info){
     return wscdnswitch_info_p.length>0;
 }
 
-//hook函数，用于修改和监控ajax请求和响应
+//初始化提示框
+function setupInfo(){
+    clearInterval(timerSetupInfo);
+    if($("#wscdnswitch_info").length>0){
+        return ;
+    }
+    timerSetupInfo = setInterval(function(){
+        if($("#wscdnswitch_info").length>0){
+            return;
+        }
+        var player = $("video");
+        //找到video标签
+        if(player.length>0){
+            //在video标签前面加入提示框
+            player.before("<div id='wscdnswitch_info' style='position:absolute; border:0px ;background-color:#00A1D6; border-radius:5px; left:10px; top:10px;'><p id='wscdnswitch_info_p' style='color:white; font-size:15px; margin:10px 10px;'>网宿CDN切换已开启</p></div>");
+            if(wscndError){
+                changeInfo("网宿CDN切换失败，请注意流量使用情况！");
+            }
+            else{
+                //3秒后缩小提示框，以免遮挡视线
+                timeOutInfo = setTimeout(function(){changeInfo("WS");}, 3000);
+            }
+            clearInterval(timerSetupInfo);
+        }
+    }, 100);
+}
+
+
+//hook，用于修改和监控ajax请求和响应
 xhook.after(function(request, response) {
     if(request.url.match(/interface\.bilibili\.com\/v2\/playurl/) ||
        request.url.match(/bangumi\.bilibili\.com\/player\/web_api\/v2\/playurl/) ||
@@ -42,6 +70,8 @@ xhook.after(function(request, response) {
                 }
             }
         }
+        setupInfo();
+        wscndError = false;
         response.text = JSON.stringify(data);
     }
     else if(request.url.match(/\/\/[^\/]+\.acgvideo\.com\//) && (request.url.match(/\/\/ws.acgvideo\.com\//)==null)){
@@ -53,41 +83,6 @@ xhook.after(function(request, response) {
         changeInfo("网宿CDN切换失败，请注意流量使用情况！");
         wscndError = true;
     }
-});
-
-//初始化提示框
-function setupInfo(count=0){
-    if($("#wscdnswitch_info").length>0){
-        return ;
-    }
-    var player = $("video");
-    //找到video标签
-    if(player.length>0){
-        //在video标签前面加入提示框
-        player.before("<div id='wscdnswitch_info' style='position:absolute; border:0px ;background-color:#00A1D6; border-radius:5px; left:10px; top:10px;'><p id='wscdnswitch_info_p' style='color:white; font-size:15px; margin:10px 10px;'>网宿CDN切换已开启</p></div>");
-        if(wscndError){
-            changeInfo("网宿CDN切换失败，请注意流量使用情况！");
-        }
-        else{
-            //3秒后缩小提示框，以免遮挡视线
-            timeOutInfo = setTimeout(function(){changeInfo("WS");}, 3000);
-        }
-    }
-    else if(count>0){
-        //若失败，且count>0重新尝试
-        setTimeout(function(){setupInfo(count-1);}, 100);
-    }
-    else{
-        //若提示框添加失败，且cdn出现错误，alert提醒。
-        if(wscndError){
-            alert("网宿CDN切换失败，请注意流量使用情况！");
-        }
-    }
-}
-
-//尝试300次初始化提示框，两次间隔100毫秒
-$(document).ready(function(){
-    setTimeout(function(){setupInfo(300);}, 100);
 });
 
 
